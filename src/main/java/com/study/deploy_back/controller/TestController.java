992de85f44ebbe31292876264a0195167ced5342
gyeolhwi <gyeori0626@gmail.com>
2024-09-27 15:26:02 +0900

    commit

--- src/main/java/com/study/deploy_back/controller/TestController.java
+++ src/main/java/com/study/deploy_back/controller/TestController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Value("${client.address}")
     // 서버 실행 될 때 yml에서 설정한 걸 가지고 올 수 있음
     // (이게 실제 서비스가 운영될 때 배포되는 react의 주소를 가지고와야하기 때문에 변수로 설정)
+    @Value("${client.address}")
     private String clientAddress;
 
-    @CrossOrigin
+    @CrossOrigin(originPatterns = "*")
     @GetMapping("/test")
     public ResponseEntity<?> test() {
         return ResponseEntity.ok(clientAddress + "테스트");
